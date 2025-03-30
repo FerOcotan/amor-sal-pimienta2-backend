93588ab4cb538d0cf15054df4c2a2d4ba03f0298
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-03-30 03:10:56 -0600

    formto json

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('/categorias', CategoriaController::class);
